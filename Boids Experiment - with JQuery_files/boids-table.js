df8654885f75d4bb51aa8afd49b202f2eda2733a
HagaiF <hagai@co-play.com>
2015-04-28 19:27:14 +0300

    sim files

---
+++ Boids Experiment - with JQuery_files/boids-table.js
@@ -0,0 +1,38 @@
+var BoidType = new Array(32);
+
+function setTable() {
+	BoidType[0] = {color: "#C3FAEB", character: "New Born", num: 0};
+	BoidType[1] = {color: "#F5C814", character: "Free Spirit", num: 0};  
+	BoidType[2] = {color: "#FF6600", character: "Individualist", num: 0}; 
+	BoidType[3] = {color: "#8844FF", character: "Egocentric", num: 0};
+	BoidType[4] = {color: "#FFFF00", character: "Cohesion", num: 0};
+	BoidType[5] = {color: "#FF0000", character: "Socialist", num: 0};
+	BoidType[6] = {color: "#AA66FF", character: "Groupist", num: 0};
+	BoidType[7] = {color: "#FF6600", character: "Flys", num: 0};
+	BoidType[8] = {color: "#8844FF", character: "Alignment", num: 0};
+	BoidType[9] = {color: "#FFFF00", character: "Hesitator", num: 0};
+	BoidType[10] = {color: "#FF0000", character: "Network", num: 0};
+	BoidType[11] = {color: "#FF6600", character: "t11", num: 0};  
+	BoidType[12] = {color: "#FF6600", character: "t12", num: 0}; 
+	BoidType[13] = {color: "#8844FF", character: "t13", num: 0};
+	BoidType[14] = {color: "#FFFF00", character: "t14", num: 0};
+	BoidType[15] = {color: "#FF0000", character: "t15", num: 0};
+	BoidType[16] = {color: "#AA66FF", character: "t16", num: 0};
+	BoidType[17] = {color: "#FF6600", character: "t17", num: 0};
+	BoidType[18] = {color: "#8844FF", character: "t18", num: 0};
+	BoidType[19] = {color: "#FFFF00", character: "t19", num: 0};
+	BoidType[20] = {color: "#FF0000", character: "t20", num: 0};
+	BoidType[21] = {color: "#FF6600", character: "t21", num: 0};  
+	BoidType[22] = {color: "#FF6600", character: "t22", num: 0}; 
+	BoidType[23] = {color: "#8844FF", character: "t23", num: 0};
+	BoidType[24] = {color: "#FFFF00", character: "t24", num: 0};
+	BoidType[25] = {color: "#FF0000", character: "t25", num: 0};
+	BoidType[26] = {color: "#AA66FF", character: "t26", num: 0};
+	BoidType[27] = {color: "#FF6600", character: "t27", num: 0};
+	BoidType[28] = {color: "#8844FF", character: "t28", num: 0};
+	BoidType[29] = {color: "#FFFF00", character: "t29", num: 0};
+	BoidType[30] = {color: "#FF0000", character: "t30", num: 0};
+	BoidType[31] = {color: "#FF6600", character: "t31", num: 0};  
+}
+
+var selectedIndex = 15;
